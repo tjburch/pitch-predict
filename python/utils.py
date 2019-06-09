1113c5d101a6638b041705884a0ed724a20af293
Tyler Burch <burcht11@gmail.com>
2019-06-09 17:55:55 -0500

    Add utils, AtBat class

---
+++ python/utils.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+
+features = [
+    "pitch_type",
+    "release_speed",
+    "description",
+    "zone",
+    "p_throws",
+    "stand",
+    "type",
+    "balls",
+    "strikes",
+    "outs_when_up",
+    "inning",
+    "release_spin_rate",
+    "at_bat_number",
+    "pitch_number",
+    "pitch_name",
+    "home_score",
+    "away_score",
+    "if_fielding_alignment",
+    "of_fielding_alignment",
+]
+
+
+class AtBat:
+    """ Class per AB, gives representation in pd.DF and numpy array """
+
+    def __init__(self, dataframe, strikeout_row):
+        self.num_pitches = dataframe["pitch_number"].iloc[strikeout_row]
+        self.df = dataframe[strikeout_row:strikeout_row+self.num_pitches].iloc[::-1]
+        self.np = self.df.to_numpy() # Shape will be (pitches, features)                
