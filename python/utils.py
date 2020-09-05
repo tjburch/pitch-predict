0c2aa1dceaaaf77c955dd5a4fa977a6a7044131d
Tyler Burch <burcht11@gmail.com>
2020-09-05 08:52:17 -0500

    Change pitch encoder to function

--- python/utils.py
+++ python/utils.py
@@ -31,36 +31,37 @@ class AtBat:
 
 
 
-class pitch_encoder:
+def pitch_encoder(encode=True):
     """Encodes pitch-type and location to index
     Args:
-        ab_array ([type]): [description]
+        encode (bool): Encode or decode pitch 
     """
-    def __init__(self):
 
-        self.pitch_map = {
-            "FF": 1, 
-            "SL": 2,
-            "FT": 3,   
-            "CH": 4,   
-            "SI": 5,   
-            "CU": 6,  
-            "FC": 7,   
-            "KC": 8,   
-            "FS": 9,   
-            "EP": 10,     
-            "FO": 11,     
-            "PO": 12,     
-            "SC": 13,
-            "KN": 14
-        }
-        self.inverse_pitch_map = {v:k for k,v in self.pitch_map.items()}
-        self.zones = np.arange(1, 15, 1) # 1-14
+    pitch_map = {
+        "FF": 1, 
+        "SL": 2,
+        "FT": 3,   
+        "CH": 4,   
+        "SI": 5,   
+        "CU": 6,  
+        "FC": 7,   
+        "KC": 8,   
+        "FS": 9,   
+        "EP": 10,     
+        "FO": 11,     
+        "PO": 12,     
+        "SC": 13,
+        "KN": 14
+    }
+    zones = np.arange(1, 15, 1) # 1-14
 
-    def encode_pitch(self, pitch_type, zone):
-        return self.pitch_map[pitch_type] * 14 + int(zone)
-
-    def decode_pitch(self, encoded_pitch):
-        pitch_type = self.inverse_pitch_map[np.floor(encoded_pitch/14)]
+    # Decide if encoding or decoding
+    if encode:
+        # Return Index
+        return (pitch_map[pitch_type] * 14 + int(zone))
+    else:
+        # Get and return type and zone
+        inverse_pitch_map = {v:k for k,v in self.pitch_map.items()}
+        pitch_type = inverse_pitch_map[np.floor(encoded_pitch/14)]
         zone = encoded_pitch % 14
         return pitch_type, zone
